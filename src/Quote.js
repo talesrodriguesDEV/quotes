d08b140d88a6fcce2c06d59ccb41869aeebea640
Tales Rodrigues <tales00001@gmail.com>
2023-02-13 11:05:25 -0300

    Implementing Quote entity

---
+++ src/Quote.js
@@ -0,0 +1,30 @@
+module.exports = function Quote ({ id, text, author, length, createdAt, updatedAt }) {
+  const missingInfo = !id || !text || !author || !length || !createdAt || !updatedAt
+  if (missingInfo) throw new Error('Some quote info is missing.')
+
+  const invalidInfoType =
+    typeof id !== 'string' ||
+    typeof text !== 'string' ||
+    typeof author !== 'string' ||
+    typeof length !== 'number' ||
+    !(createdAt instanceof Date) ||
+    !(updatedAt instanceof Date)
+  if (invalidInfoType) throw new Error('Invalid info type.')
+
+  if (text.length > 100 || text.length < 10) throw new Error('Text must have at least 10 characters and maximum 100.')
+
+  if (author.length > 20 || author.length < 3) throw new Error('Author must have at least 3 characters and maximum 20.')
+
+  if (text.length !== length) throw new Error('Invalid quote length.')
+
+  if (createdAt > updatedAt) throw new Error('Invalid dates.')
+
+  return Object.freeze({
+    id,
+    text,
+    author,
+    length,
+    createdAt,
+    updatedAt
+  })
+}
