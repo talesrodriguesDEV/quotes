2083938d5b35b89b063a85198490f8cf91a894bb
Tales Rodrigues <tales00001@gmail.com>
2023-02-15 18:49:02 -0300

    Entities unit tests

---
+++ src/entities/Author.test.js
@@ -0,0 +1,47 @@
+const Author = require('./Author')
+
+const mockAuthor = { id: '1', name: 'name', age: 21, country: 'country', createdAt: new Date(), updatedAt: new Date() }
+
+describe('Testing Author entity', () => {
+  it('should throw an Error for missing Author info', () => {
+    expect(() => Author({})).toThrow('Some author info is missing.')
+  })
+
+  it('should throw an Error for invalid info type', () => {
+    expect(() => Author({ ...mockAuthor, createdAt: 1 })).toThrow('Invalid info type.')
+  })
+
+  it('should throw an Error for invalid name length', () => {
+    expect(() => Author({ ...mockAuthor, name: 'Ta' })).toThrow('Name must have at least 3 characters and maximum 20.')
+  })
+
+  it('should throw an Error for invalid author age', () => {
+    expect(() => Author({ ...mockAuthor, age: 17 })).toThrow('Authors must be at least 18 years old.')
+  })
+
+  it('should throw an Error for invalid country length', () => {
+    expect(() => Author({ ...mockAuthor, country: 'United States of America' })).toThrow('Country must have at least 3 characters and maximum 10.')
+  })
+
+  it('should throw an Error for invalid dates', () => {
+    expect(() => Author({ ...mockAuthor, updatedAt: new Date('2023-02-15T21:24:12.523Z') })).toThrow('Invalid dates.')
+  })
+
+  it('should return an Author object sucessfully', () => {
+    const author = Author(mockAuthor)
+
+    expect(author).toEqual(Object.freeze(mockAuthor))
+  })
+
+  it('should throw an Error when an attempt to change Author ocurr', () => {
+    'use strict'
+
+    const author = Author(mockAuthor)
+
+    const changeCountry = () => {
+      author.country = 'a'
+    }
+
+    expect(changeCountry).toThrow()
+  })
+})
---
+++ src/entities/Quote.test.js
@@ -0,0 +1,47 @@
+const Quote = require('./Quote')
+
+const mockQuote = { id: '1', text: 'What a great text!', author: 'author', length: 18, createdAt: new Date(), updatedAt: new Date() }
+
+describe('Testing Quote entity', () => {
+  it('should throw an Error for missing Quote info', () => {
+    expect(() => Quote({})).toThrow('Some quote info is missing.')
+  })
+
+  it('should throw an Error for invalid info type', () => {
+    expect(() => Quote({ ...mockQuote, id: 1 })).toThrow('Invalid info type.')
+  })
+
+  it('should throw an Error for invalid text length', () => {
+    expect(() => Quote({ ...mockQuote, text: 'nice text' })).toThrow('Text must have at least 10 characters and maximum 100.')
+  })
+
+  it('should throw an Error for invalid author length', () => {
+    expect(() => Quote({ ...mockQuote, author: 'au' })).toThrow('Author must have at least 3 characters and maximum 20.')
+  })
+
+  it('should throw an Error for invalid text length', () => {
+    expect(() => Quote({ ...mockQuote, length: 20 })).toThrow('Invalid quote length.')
+  })
+
+  it('should throw an Error for invalid dates', () => {
+    expect(() => Quote({ ...mockQuote, updatedAt: new Date('2023-02-15T21:24:12.523Z') })).toThrow('Invalid dates.')
+  })
+
+  it('should return an Quote object sucessfully', () => {
+    const quote = Quote(mockQuote)
+
+    expect(quote).toEqual(Object.freeze(mockQuote))
+  })
+
+  it('should throw an Error when an attempt to change Quote ocurr', () => {
+    'use strict'
+
+    const quote = Quote(mockQuote)
+
+    const changeAuthor = () => {
+      quote.author = 'Bob'
+    }
+
+    expect(changeAuthor).toThrow()
+  })
+})
